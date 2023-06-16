7bb881744729288e51c3be26b078a3c5529fc91e
shiponcs <matin@appscode.com>
2023-06-16 09:37:19 +0600

    implement PostBook method

--- server.cc
+++ server.cc
@@ -1,16 +1,25 @@
 #include <grpcpp/grpcpp.h>
 #include <string>
+#include <iostream>
+#include <map>
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include "bookstore.grpc.pb.h"
+#include <cstdlib>
 
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
+using grpc::ServerWriter;
 using grpc::Status;
 
 using bookstore::BookStore;
 using bookstore::Empty;
 using bookstore::HellowReply;
+using bookstore::Book;
+using bookstore::Reply;
+
+
+std::map< int, Book > db;
 
 class BookStoreServiceImpl final : public BookStore::Service {
 	Status SayHello(ServerContext* context, const Empty* request, HellowReply* reply) override {
@@ -18,6 +27,25 @@ class BookStoreServiceImpl final : public BookStore::Service {
 		reply->set_message(ourReply);
 		return Status::OK;
 	}
+	
+	Status GetAllBooks(ServerContext* context, const Empty* request, ServerWriter<Book>* writer) override {
+		Book book;
+		book.set_id(5);
+		book.set_name("GEB");
+		book.set_author("ABC");
+		
+		writer->Write(book);
+		book.set_id(33);
+		writer->Write(book);
+		
+		return Status::OK;
+	}
+
+    Status PostBook(ServerContext* context, const Book* request, Reply* reply) override {
+        std::cout << "PostBook: " << request->id() << " " << request->name() << " " << request->author() <<"\n";
+        reply->set_msg("Book created with id: ");
+        return  Status::OK;
+    }
 };
 
 void RunServer() {
